e46640ad71fe0ff7ed51d929dfe28f5ea9618dc2
Marcello Vaz <7875005+mcrvaz@users.noreply.github.com>
2022-12-04 18:03:59 -0300

    feat: day 4 part 2

--- src/days/day4/part2.rs
+++ src/days/day4/part2.rs
@@ -1,31 +1,67 @@
-use std::{collections::HashSet, fs::read_to_string};
+use std::fs::read_to_string;
 
 pub fn solve() {
     let result = internal_solve("src/days/day4/input.txt");
     println!("Result: {}", result);
 }
 
+#[derive(Clone, Copy)]
+struct Range {
+    start: i32,
+    end: i32,
+}
+
+impl Range {
+    fn from_str(range: &str) -> Self {
+        let mut pair = range.split('-');
+        let first_start: i32 = pair.next().unwrap().parse().unwrap();
+        let first_end: i32 = pair.next().unwrap().parse().unwrap();
+        Range::new(first_start, first_end)
+    }
+
+    fn new(start: i32, end: i32) -> Self {
+        Range {
+            start: start,
+            end: end,
+        }
+    }
+
+    fn partial_contains(self, other: Range) -> bool {
+        self.start <= other.end && self.end >= other.start
+    }
+}
+
 fn internal_solve(path: &str) -> i32 {
-    -1
+    let content = read_to_string(path).expect("Fail to read file.");
+    let mut count = 0;
+    for line in content.lines() {
+        let mut pairs = line.split(',');
+        let first_range = Range::from_str(pairs.next().unwrap());
+        let second_range = Range::from_str(pairs.next().unwrap());
+        if first_range.partial_contains(second_range) {
+            count += 1;
+        }
+    }
+    count
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    // #[test]
-    // fn sample() {
-    //     const PATH: &str = "src/days/day3/test-input.txt";
-    //     const EXPECTED: i32 = 157;
-    //     let result = internal_solve(PATH);
-    //     assert_eq!(result, EXPECTED);
-    // }
+    #[test]
+    fn sample() {
+        const PATH: &str = "src/days/day4/test-input.txt";
+        const EXPECTED: i32 = 4;
+        let result = internal_solve(PATH);
+        assert_eq!(result, EXPECTED);
+    }
 
-    // #[test]
-    // fn result() {
-    //     const PATH: &str = "src/days/day3/input.txt";
-    //     const EXPECTED: i32 = 8105;
-    //     let result = internal_solve(PATH);
-    //     assert_eq!(result, EXPECTED);
-    // }
+    #[test]
+    fn result() {
+        const PATH: &str = "src/days/day4/input.txt";
+        const EXPECTED: i32 = 905;
+        let result = internal_solve(PATH);
+        assert_eq!(result, EXPECTED);
+    }
 }
