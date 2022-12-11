8353cc725fee71f214f41b7fa4a1d9ac8cb8811d
Marcello Vaz <7875005+mcrvaz@users.noreply.github.com>
2022-12-10 21:44:47 -0300

    feat: day 8 part 2

--- src/days/day8/part2.rs
+++ src/days/day8/part2.rs
@@ -1,184 +1,100 @@
-use lazy_static::lazy_static;
-use regex::Regex;
-use std::{cell::RefCell, fs::read_to_string, rc::Rc};
+use std::fs::read_to_string;
 
 pub fn solve() {
-    let result = internal_solve("src/days/day7/input.txt");
+    let result = internal_solve("src/days/day8/input.txt");
     println!("Result: {}", result);
 }
 
-fn internal_solve(path: &str) -> i32 {
+fn internal_solve(path: &str) -> u32 {
     let content = read_to_string(path).expect("Fail to read file.");
-    let tree = parse(&content);
-    get_result(tree.root.clone())
+    let trees = parse_trees(&content);
+    get_highest_score(&trees)
 }
 
-fn get_result(root_ref: Rc<RefCell<TreeNode>>) -> i32 {
-    const TOTAL_SIZE: i32 = 70000000;
-    const TARGET_SIZE: i32 = 30000000;
-    let root = root_ref.borrow();
-    let available_space = TOTAL_SIZE - root.size;
-    let space_needed = TARGET_SIZE - available_space;
-
-    let mut sizes = Vec::new();
-    get_sizes(root_ref.clone(), &mut sizes);
-    fn get_sizes(root_ref: Rc<RefCell<TreeNode>>, vec: &mut Vec<i32>) {
-        let node = root_ref.borrow();
-        vec.push(node.size);
-        for c in root_ref.borrow().children.iter() {
-            get_sizes(c.clone(), vec);
+fn parse_trees(content: &str) -> Vec<Vec<u32>> {
+    let mut trees: Vec<Vec<u32>> = Vec::with_capacity(content.lines().count());
+    for (i, line) in content.lines().enumerate() {
+        trees.insert(i, Vec::with_capacity(line.len()));
+        for (j, c) in line.chars().enumerate() {
+            trees[i].insert(j, c.to_digit(10).unwrap());
         }
     }
-
-    sizes
-        .iter()
-        .filter(|x| **x >= space_needed)
-        .min()
-        .map(|x| *x)
-        .unwrap()
+    trees
 }
 
-fn parse(content: &str) -> Tree {
-    let tree = Tree::new();
-    let mut tree_pointer = tree.root.clone();
-    for line in content.lines() {
-        if let Some(param) = parse_cd(&line) {
-            match param {
-                "/" => {
-                    continue;
-                }
-                ".." => {
-                    move_to_upper_dir(&mut tree_pointer);
-                }
-                p => {
-                    let target = tree_pointer.borrow().find_child(p).unwrap();
-                    tree_pointer = target;
-                }
+fn get_highest_score(trees: &Vec<Vec<u32>>) -> u32 {
+    let mut highest_score = 0;
+    for row in 0..trees.len() {
+        for col in 0..trees[row].len() {
+            let score = get_scenic_score(row, col, &trees);
+            if score > highest_score {
+                highest_score = score;
             }
-        } else if parse_ls(&line) {
-            continue;
-        } else if let Some(dir) = parse_dir(&line) {
-            tree_pointer
-                .borrow_mut()
-                .add_child(tree_pointer.clone(), &dir.name);
-        } else if let Some(file) = parse_file(&line) {
-            tree_pointer.borrow_mut().increase_size(file.size)
-        } else {
-            panic!("Invalid command!");
         }
     }
-
-    while tree_pointer.borrow().parent.is_some() {
-        move_to_upper_dir(&mut tree_pointer);
-    }
-    tree
+    highest_score
 }
 
-fn move_to_upper_dir(tree_pointer: &mut Rc<RefCell<TreeNode>>) {
-    let current_size = tree_pointer.borrow().size.clone();
-    let parent = tree_pointer.borrow().parent.clone().unwrap();
-    parent.borrow_mut().increase_size(current_size);
-    *tree_pointer = parent;
+fn get_scenic_score(row: usize, col: usize, trees: &Vec<Vec<u32>>) -> u32 {
+    get_top_score(row, col, trees)
+        * get_bot_score(row, col, trees)
+        * get_left_score(row, col, trees)
+        * get_right_score(row, col, trees)
 }
 
-fn parse_cd(line: &str) -> Option<&str> {
-    lazy_static! {
-        static ref CD_RX: Regex = Regex::new(r"\$ cd (\S+)").unwrap();
-    }
-
-    if let Some(c) = CD_RX.captures(line) {
-        return c.get(1).and_then(|x| Some(x.as_str()));
-    }
-    None
-}
-
-fn parse_ls(line: &str) -> bool {
-    line == "$ ls"
-}
-
-fn parse_dir(line: &str) -> Option<Directory> {
-    lazy_static! {
-        static ref DIR_RX: Regex = Regex::new(r"dir (\S+)").unwrap();
-    }
-
-    if let Some(c) = DIR_RX.captures(line) {
-        return c.get(1).and_then(|x| Some(x.as_str())).and_then(|x| {
-            Some(Directory {
-                name: x.to_string(),
-            })
-        });
-    }
-    None
-}
-
-fn parse_file(line: &str) -> Option<File> {
-    lazy_static! {
-        static ref FILE_RX: Regex = Regex::new(r"(\d+) (\S+)").unwrap();
-    }
-
-    if let Some(c) = FILE_RX.captures(line) {
-        let size = c
-            .get(1)
-            .and_then(|x| Some(x.as_str()))
-            .and_then(|x| Some(x.parse().unwrap()));
-        let name = c.get(2).and_then(|x| Some(x.as_str()));
-        if let (Some(size), _) = (size, name) {
-            return Some(File { size });
+fn get_top_score(row: usize, col: usize, trees: &Vec<Vec<u32>>) -> u32 {
+    let value = trees[row][col];
+    let mut score = 0;
+    for i in (0..row).rev() {
+        let current = trees[i][col];
+        score += 1;
+        if current >= value {
+            return score;
         }
     }
-    None
+    score
 }
 
-struct Tree {
-    root: Rc<RefCell<TreeNode>>,
-}
-
-struct TreeNode {
-    size: i32,
-    name: String,
-    children: Vec<Rc<RefCell<TreeNode>>>,
-    parent: Option<Rc<RefCell<TreeNode>>>,
-}
-
-impl Tree {
-    fn new() -> Self {
-        let root = Rc::new(RefCell::new(TreeNode {
-            size: 0,
-            parent: None,
-            name: String::from("/"),
-            children: Vec::new(),
-        }));
-        Tree { root }
+fn get_bot_score(row: usize, col: usize, trees: &Vec<Vec<u32>>) -> u32 {
+    let value = trees[row][col];
+    let row_count = trees.len();
+    let mut score = 0;
+    for i in (row + 1)..row_count {
+        let current = trees[i][col];
+        score += 1;
+        if current >= value {
+            return score;
+        }
     }
+    score
 }
 
-impl TreeNode {
-    fn add_child(&mut self, parent: Rc<RefCell<TreeNode>>, name: &str) {
-        let node = Rc::new(RefCell::new(TreeNode {
-            size: 0,
-            name: name.to_string(),
-            parent: Some(parent),
-            children: Vec::new(),
-        }));
-        self.children.push(node);
+fn get_left_score(row: usize, col: usize, trees: &Vec<Vec<u32>>) -> u32 {
+    let value = trees[row][col];
+    let mut score = 0;
+    for i in (0..col).rev() {
+        let current = trees[row][i];
+        score += 1;
+        if current >= value {
+            return score;
+        }
     }
-
-    fn find_child(&self, name: &str) -> Option<Rc<RefCell<TreeNode>>> {
-        let target = self.children.iter().find(|x| x.borrow().name == name);
-        target.map(|x| x.clone())
-    }
-
-    fn increase_size(&mut self, incr: i32) {
-        self.size += incr;
-    }
+    score
 }
 
-struct File {
-    size: i32,
-}
+fn get_right_score(row: usize, col: usize, trees: &Vec<Vec<u32>>) -> u32 {
+    let value = trees[row][col];
+    let col_count = trees[row].len();
+    let mut score = 0;
 
-struct Directory {
-    name: String,
+    for i in (col + 1)..col_count {
+        let current = trees[row][i];
+        score += 1;
+        if current >= value {
+            return score;
+        }
+    }
+    score
 }
 
 #[cfg(test)]
@@ -187,17 +103,16 @@ mod tests {
 
     #[test]
     fn sample() {
-        const PATH: &str = "src/days/day7/test-input.txt";
-        const EXPECTED: i32 = 24933642;
+        const PATH: &str = "src/days/day8/test-input.txt";
+        const EXPECTED: u32 = 8;
         let result = internal_solve(PATH);
         assert_eq!(result, EXPECTED);
     }
 
     #[test]
     fn result() {
-        println!("my version");
-        const PATH: &str = "src/days/day7/input.txt";
-        const EXPECTED: i32 = 3636703;
+        const PATH: &str = "src/days/day8/input.txt";
+        const EXPECTED: u32 = 470596;
         let result = internal_solve(PATH);
         assert_eq!(result, EXPECTED);
     }
