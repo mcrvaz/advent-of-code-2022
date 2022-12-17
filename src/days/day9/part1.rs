a5820b210d00f37b986d91ac22f969f63e1243f1
Marcello Vaz <7875005+mcrvaz@users.noreply.github.com>
2022-12-17 17:40:31 -0300

    feat: day 9 part 1

--- src/days/day9/part1.rs
+++ src/days/day9/part1.rs
@@ -1,8 +1,4 @@
-use std::{
-    collections::HashSet,
-    fs::read_to_string,
-    ops::{Add, Mul},
-};
+use std::{collections::HashSet, fs::read_to_string, ops::Add};
 
 pub fn solve() {
     let result = internal_solve("src/days/day9/input.txt");
@@ -12,11 +8,7 @@ pub fn solve() {
 fn internal_solve(path: &str) -> usize {
     let content = read_to_string(path).expect("Fail to read file.");
     let commands = parse(&content);
-    let mut tail_positions: Vec<Point> = vec![Point::ZERO];
-    let mut head_pos: Point = Point::ZERO;
-    for c in commands {
-        head_pos = c.exec(&head_pos, &mut tail_positions);
-    }
+    let tail_positions = compute_tail_positions(&commands);
     let unique_positions: HashSet<Point> = HashSet::from_iter(tail_positions.iter().cloned());
     unique_positions.iter().count()
 }
@@ -25,7 +17,16 @@ fn parse(content: &str) -> Vec<Command> {
     content.lines().map(|x| Command::from_str(x)).collect()
 }
 
-#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+fn compute_tail_positions(commands: &Vec<Command>) -> Vec<Point> {
+    let mut tail_positions: Vec<Point> = vec![Point::ZERO];
+    let mut head_pos: Point = Point::ZERO;
+    for c in commands {
+        head_pos = c.exec(&head_pos, &mut tail_positions);
+    }
+    tail_positions
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
 struct Point {
     x: i32,
     y: i32,
@@ -53,8 +54,8 @@ impl Point {
     }
 
     fn is_adjacent(&self, other: &Self) -> bool {
-        const MAX_DIST: i32 = 1;
-        (self.x - other.x) <= MAX_DIST && (self.y - other.y) <= MAX_DIST
+        const MAX_DIST: u32 = 1;
+        self.x.abs_diff(other.x) <= MAX_DIST && self.y.abs_diff(other.y) <= MAX_DIST
     }
 }
 
@@ -62,13 +63,6 @@ impl Add for Point {
     type Output = Self;
     fn add(self, rhs: Self) -> Self {
         Self::new(self.x + rhs.x, self.y + rhs.y)
-    }
-}
-
-impl Mul<i32> for Point {
-    type Output = Self;
-    fn mul(self, rhs: i32) -> Self {
-        Self::new(self.x * rhs, self.y * rhs)
     }
 }
 
@@ -87,31 +81,34 @@ impl Command {
     }
 
     fn exec(&self, head_pos: &Point, tail_positions: &mut Vec<Point>) -> Point {
-        fn get_new_tail_position(head: &Point, current_tail: &Point) -> Point {
-            const MAX_DIST: i32 = 1;
+        fn get_new_tail_position(head: &Point, current_tail: &Point) -> Option<Point> {
             if head.is_adjacent(current_tail) {
-                return *current_tail;
-            }
-            let mut new_tail = *current_tail;
-            let x_dist = head.x - current_tail.x;
-            let y_dist = head.y - current_tail.y;
-            if x_dist.abs() > MAX_DIST {
-                new_tail.x += x_dist
+                return None;
             }
 
-            if y_dist.abs() > MAX_DIST {
-                new_tail.y += x_dist
+            let y_diff_sig = (head.y - current_tail.y).signum();
+            let x_diff_sig = (head.x - current_tail.x).signum();
+            let mut x = 0;
+            let mut y = 0;
+            if head.x == current_tail.x {
+                y = y_diff_sig;
+            } else if head.y == current_tail.y {
+                x = x_diff_sig;
+            } else {
+                y = y_diff_sig;
+                x = x_diff_sig;
             }
-
-            new_tail
+            let new_tail = *current_tail + Point::new(x, y);
+            Some(new_tail)
         }
 
         let mut new_head_pos = *head_pos;
         for _ in 0..self.count {
             new_head_pos = new_head_pos + self.dir;
             let previous_tail_pos = tail_positions.last().unwrap();
-            let new_tail_pos = get_new_tail_position(&new_head_pos, previous_tail_pos);
-            tail_positions.push(new_tail_pos);
+            if let Some(p) = get_new_tail_position(&new_head_pos, previous_tail_pos) {
+                tail_positions.push(p);
+            }
         }
         new_head_pos
     }
@@ -130,9 +127,34 @@ mod tests {
     }
 
     #[test]
+    fn sample_path() {
+        const PATH: &str = "src/days/day9/test-input.txt";
+        let content = read_to_string(PATH).expect("Fail to read file.");
+        let commands = parse(&content);
+        let expected = vec![
+            Point::new(0, 0),
+            Point::new(1, 0),
+            Point::new(2, 0),
+            Point::new(3, 0),
+            Point::new(4, 1),
+            Point::new(4, 2),
+            Point::new(4, 3),
+            Point::new(3, 4),
+            Point::new(2, 4),
+            Point::new(3, 3),
+            Point::new(4, 3),
+            Point::new(3, 2),
+            Point::new(2, 2),
+            Point::new(1, 2),
+        ];
+        let result = compute_tail_positions(&commands);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
     fn result() {
         const PATH: &str = "src/days/day9/input.txt";
-        const EXPECTED: usize = 0;
+        const EXPECTED: usize = 6087;
         let result = internal_solve(PATH);
         assert_eq!(result, EXPECTED);
     }
